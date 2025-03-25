fc7c65f20227434137132253c644da834ac819a9
Faizan Ali <engr.faizanaliansari@gmail.com>
2025-03-26 01:06:40 +0400

    adding production url

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS for all origins
+  const isProduction = process.env.NODE_ENV === 'production';
   app.enableCors({
-    origin: 'http://localhost:3001', // Allow requests from Next.js running on port 3001
+    origin: isProduction ? 'https://webapp-demo1-ahdgf5e6ghe9aefb.uaenorth-01.azurewebsites.net' : 'http://localhost:3001', // Use your frontend app URL for production
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
   await app.listen(process.env.PORT || 8080);
