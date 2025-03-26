c0a9c0f02b31f6380e085f4fbbfcfa596c5a0230
Faizan Ali <engr.faizanaliansari@gmail.com>
2025-03-26 23:39:13 +0400

    new frontend url added

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   // Enable CORS for all origins
   const isProduction = process.env.NODE_ENV === 'production';
   app.enableCors({
-    origin: isProduction ? 'https://webapp-demo1-ahdgf5e6ghe9aefb.uaenorth-01.azurewebsites.net' : 'http://localhost:3001', // Use your frontend app URL for production
+    origin: isProduction ? 'https://nextjs-frontend-hhd3gahrfeccezfy.uaenorth-01.azurewebsites.net/' : 'http://localhost:3001', // Use your frontend app URL for production
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
   });
   await app.listen(process.env.PORT || 8080);
